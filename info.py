83e511c8a03d7a2588fbbc5e04ebf4ea7947e244
Sonu dhakad <145421908+Sonudhakad786@users.noreply.github.com>
2024-10-30 19:14:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
+API_ID       = 27696177
+API_HASH     = "0c44906a4feff3b947db76dfa7c57d88"
+BOT_TOKEN    = "5918038913:AAEJs4WeXqlDJ7NQmv698oOD-9RZ9L2Hcjc"
+SESSION      = "BQHFENsAEiukToePj9t46L5k9R8Prt_VDDFdvyJjCwBQGml1RpcKM9nsj1qei5d2uPZsQGFRM6ENs5VnpBVK4l76odHnuVE3x0QbnF8O0-B64rYS9nsVH12YMelGEfHA5PN8MglwLadH0iFbmv-G4SpvL73XLSv4Pob2lh7imrWZ8sC1heMFYlRnQjOBQabcIznVADHNlLMVQvHYB9i851u3iW4MxkZjoCLEdtykyoksqYbvk1uElDQij5r4TDsCl_OiTyS5SJSc3veMgIsH7SsCUSApYGqyjLSU7ZNwaEgkb3IT9kv2jIj2RlN-7Ay1VrcYFBEqftz5lBbaFFZj912mvTEYcgAAAAAyy2V9AA"
+DATABASE_URI = "mongodb+srv://Spiderman:ohWHjNXG9VedoC5R@cluster0.uuauf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002116984433
+ADMIN        = 852190589
 CHANNEL      = "RahulReviewsYT"
